8ba9e3bc0464facf37541b4f449a42c21740867c
Jonathan Honda <hondahelix120@gmail.com>
2020-10-08 18:11:34 -0700

    changed the questions and made them and object

--- index.js
+++ index.js
@@ -5,24 +5,23 @@ let generateMarkdown = require('./utils/generateMarkdown.js');
 const writeFileAsync = util.promisify(fs.writeFile);
 //console.log(generateMarkdown());
 
-const questions = [
-    {
-        type: "input",
-        message: "What is your Github username?",
-        name: "username"
-    },
-    {
-        type: "input",
-        message: "What is your projects title?",
-        name: "title"
-    }
-
-
-];
+function Questions(userPrompt,userInput){
+    this.type = "input",
+    this.message = userPrompt,
+    this.name = userInput;
+}
+const questions = ["What is your Github username?","What is your projects title?","What is the description of this project?"];
+const input = ["gitHubUsername", "title","description"]
+const prompt = [];
+for(i=0; i<questions.length; i++){
+    prompt.push(new Questions(questions[i],input[i]));
+}
+//console.log(prompt);
 
 async function promptUser(){
     try{
-        const response = await inquirer.prompt(questions);
+        const response = await inquirer.prompt(prompt);
+        console.log(response);
         const filledFile = generateMarkdown(response);
         console.log(filledFile);
     }
