faa4ef1b152453f29807306b40c2022f00995379
Jonathan Honda <hondahelix120@gmail.com>
2020-10-08 17:37:38 -0700

    added outoutline of readme

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+let generateMarkdown = require('./utils/generateMarkdown.js');
+const writeFileAsync = util.promisify(fs.writeFile);
+//console.log(generateMarkdown());
+
 const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your projects title?",
+        name: "title"
+    }
+
 
 ];
 
+async function promptUser(){
+    try{
+        const response = await inquirer.prompt(questions);
+        const filledFile = generateMarkdown(response);
+        console.log(filledFile);
+    }
+    catch (err){
+        console.log(err);
+    }
+}
+//const filledFile = generateMarkdown(response)
 function writeToFile(fileName, data) {
 }
+promptUser();
+// function init() {
 
-function init() {
+// }
 
-}
-
-init();
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,34 @@
 function generateMarkdown(data) {
-  return `
-# ${data.title}
+     return `
+   # ${data.title}
 
-`;
+   ## Description 
+
+   ${data.description}
+
+   ## Table of Contents
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [Credits](#credits)
+   * [License](#license)
+   
+   ## Installation
+
+   ## Usage 
+
+   ## Credits
+
+   ## License
+
+   ## Badges
+
+   ## Contributing
+
+   ## Tests
+
+   ## Questions
+
+   `;
 }
 
 module.exports = generateMarkdown;
